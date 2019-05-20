1819096b25ac7a6a5bcfa39f2bc3a96709b771bd
Albert Sverdlov <sverdlovalbert@gmail.com>
2019-05-21 00:41:10 +0300

    Extend middleware example

--- examples/middleware.lua
+++ examples/middleware.lua
@@ -3,6 +3,7 @@ local http_router = require('http.router')
 local http_server = require('http.server')
 local tsgi = require('http.tsgi')
 local json = require('json')
+local log = require('log')
 
 box.cfg{}  -- luacheck: ignore
 
@@ -11,56 +12,60 @@ local httpd = http_server.new('127.0.0.1', 12345, {
     log_errors = true
 })
 
--- TODO
---[[
-local function preroute_middleware_1(env)
+local function swap_orange_and_apple(env)
+    local path_info = env['PATH_INFO']
+    log.info('swap_orange_and_apple: path_info = %s', path_info)
+    if path_info == '/fruits/orange' then
+        env['PATH_INFO'] = '/fruits/apple'
+    elseif path_info == '/fruits/apple' then
+        env['PATH_INFO'] = '/fruits/orange'
+    end
 
-end
---]]
-
--- TODO: maybe route-metadata is passed here also?
---
--- Of course, only generic one can be applied,
--- since middleware shouldn't be tied to any
--- specific router.
---
-local function middleware_1(env)
-    env['session_id'] = 'abacaba'
     local handler = tsgi.next_handler(env)
     return handler(env)
 end
 
-local function middleware_2(env)
-    if env['session_id'] == 'abacaba' then
-        env['authenticated'] = true
-    end
+local function add_helloworld_to_response(env)
     local handler = tsgi.next_handler(env)
     local resp = handler(env)
+
+    local lua_body = json.decode(resp.body)
+    lua_body.message = 'hello world!'
+    resp.body = json.encode(lua_body)
+
     return resp
 end
 
-local function userdefined_handler(request)
-    local authenticated = request.env['authenticated']
-    if authenticated then
-        return {
-            status = 200, body = json.encode({authenticated = 'yes'})
-        }
-    end
-    return {
-        status = 200, body = json.encode({authenticated = 'no'})
-    }
+local function apple_handler(_)
+    return {status = 200, body = json.encode({kind = 'apple'})}
 end
 
-local router = http_router.new(httpd) -- luacheck: ignore
+local function orange_handler(_)
+    return {status = 200, body = json.encode({kind = 'orange'})}
+end
+
+local _ = http_router.new(httpd, {
+        preroute_middleware = {
+            swap_orange_and_apple,
+        }
+    }) -- luacheck: ignore
     :route({
             method = 'GET',
             path = '/fruits/apple',
             middleware = {
-                middleware_1,
-                middleware_2,
+                add_helloworld_to_response,
             }
         },
-        userdefined_handler
+        apple_handler
+    )
+    :route({
+            method = 'GET',
+            path = '/fruits/orange',
+            middleware = {
+                add_helloworld_to_response,
+            }
+        },
+        orange_handler
     )
 
 httpd:start()
